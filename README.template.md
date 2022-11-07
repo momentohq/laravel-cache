8aeea2c983201b19446ef6d543515a52c64b9a2a
Erika Tharp <eem@momentohq.com>
2022-11-07 10:39:25 -0800

    feat: initial commit to move laravel-cache to momentohq (#1)

---
+++ README.template.md
@@ -0,0 +1,98 @@
+{{ ossHeader }}
+
+## Getting Started :running:
+
+### Requirements
+
+- A Momento Auth Token is required, you can generate one using
+  the [Momento CLI](https://github.com/momentohq/momento-cli)
+- At least PHP 8.0
+- Laravel 9.x
+- The grpc PHP extension. See the [gRPC docs](https://github.com/grpc/grpc/blob/v1.46.3/src/php/README.md) section on
+  installing the extension.
+
+**IDE Notes**: You'll most likely want to use an IDE that supports PHP development, such
+as [PhpStorm](https://www.jetbrains.com/phpstorm/) or [Microsoft Visual Studio Code](https://code.visualstudio.com/).
+
+### Examples
+
+Check out full working code in [the example app](https://github.com/momentohq/laravel-example)!
+
+### Installation
+
+Install composer [as described on the composer website](https://getcomposer.org/doc/00-intro.md).
+
+Add our repository to your `composer.json` file and our SDK as a dependency:
+
+```json
+{
+  "repositories": [
+    {
+      "type": "vcs",
+      "url": "https://github.com/momentohq/laravel-cache"
+    }
+  ],
+  "require": {
+    "momentohq/laravel-cache": "0.1.0"
+  }
+}
+```
+
+Run `composer update` to install the necessary prerequisites.
+
+Then, add `MomentoServiceProvider` to your `config/app.php`:
+
+```php
+'providers' => [
+    // ...
+    Momento\Cache\MomentoServiceProvider::class
+];
+```
+
+Finally, add the required config to your `config/cache.php`:
+
+```php
+'default' => env('CACHE_DRIVER', 'momento'),
+
+'stores' => [
+        'momento' => [
+            'driver' => 'momento',
+            'cache_name' => env('MOMENTO_CACHE_NAME'),
+            'default_ttl' => 60,
+        ],
+		// ...
+],
+```
+
+Run `composer update` to install the necessary prerequisites.
+
+### Usage
+
+Check out full working code in [the example app](https://github.com/momentohq/laravel-example)!
+
+### Tuning
+
+Coming soon!
+
+### Tag Support
+
+Cache tags work the same way as explained in the Laravel [documentation](https://laravel.com/docs/9.x/cache#cache-tags).
+
+**Notes**: If a tag may contain more than 4MB worth of keys, tagging may not work as intended.
+Please contact us at support@momentohq.com or on our [Discord](https://discord.com/invite/3HkAKjUZGq) if you need
+support for a larger tag set.
+
+### Unsupported Cache Operations
+
+The following cache operations are not supported today.
+If you need these operations, please reach out to us, and we can prioritize the work to complete them.
+You can file a GitHub issue, e-mail us at support@momentohq.com, or join
+our [Discord](https://discord.com/invite/3HkAKjUZGq).
+
+- `many`
+- `putMany`
+- `decrement`
+- `forever`
+- `flush`
+
+{{ ossFooter }}
---
+++ src/MomentoStore.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Momento\Cache;
+
+use Illuminate\Cache\TaggableStore;
+use Illuminate\Cache\TagSet;
+use Momento\Auth\EnvMomentoTokenProvider;
+use Momento\Cache\Errors\UnknownError;
+
+class MomentoStore extends TaggableStore
+{
+    protected SimpleCacheClient $client;
+    protected string $cacheName;
+
+    public function __construct(string $cacheName, int $defaultTtl)
+    {
+        $authProvider = new EnvMomentoTokenProvider('MOMENTO_AUTH_TOKEN');
+        $this->client = new SimpleCacheClient($authProvider, $defaultTtl);
+        $this->cacheName = $cacheName;
+        $this->client->createCache($cacheName);
+    }
+
+    public function get($key)
+    {
+        $result = $this->client->get($this->cacheName, $key);
+        if ($result->asHit()) {
+            return $result->asHit()->value();
+        } elseif ($result->asMiss()) {
+            return null;
+        }
+    }
+
+    public function many(array $keys)
+    {
+        throw new UnknownError("many operations is currently not supported.");
+    }
+
+    public function put($key, $value, $seconds)
+    {
+        $result = $this->client->set($this->cacheName, $key, $value, $seconds);
+        if ($result->asSuccess()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function putMany(array $values, $seconds)
+    {
+        throw new UnknownError("putMany operations is currently not supported.");
+    }
+
+    public function increment($key, $value = 1)
+    {
+        $getResult = $this->client->get($this->cacheName, $key);
+        if ($getResult->asHit()) {
+            $incrementedValue = intval($getResult->asHit()->value()) + 1;
+            $result = $this->client->set($this->cacheName, $key, $incrementedValue);
+            if ($result->asSuccess()) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if ($getResult->asMiss()) {
+            $result = $this->client->set($this->cacheName, $key, 0);
+            if ($result->asSuccess()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public function decrement($key, $value = 1)
+    {
+        throw new UnknownError("decrement operations is currently not supported.");
+    }
+
+    public function forever($key, $value)
+    {
+        throw new UnknownError("forever operations is currently not supported.");
+    }
+
+    public function forget($key)
+    {
+        $result = $this->client->delete($this->cacheName, $key);
+        if ($result->asSuccess()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function flush()
+    {
+        throw new UnknownError("flush operations is currently not supported.");
+    }
+
+    public function getPrefix()
+    {
+    }
+
+    public function setAdd(string $cacheName, string $setName, string $element, bool $refreshTtl, ?int $ttlSeconds = null)
+    {
+        $result = $this->client->setAdd($cacheName, $setName, $element, $refreshTtl, $ttlSeconds);
+        if ($result->asSuccess()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setFetch(string $cacheName, string $setName)
+    {
+        $result = $this->client->setFetch($cacheName, $setName);
+        if ($result->asHit()) {
+            return $result->asHit()->stringSet();
+        } else {
+            return null;
+        }
+    }
+
+    public function tags($names)
+    {
+        return new MomentoTaggedCache(
+            $this, new TagSet($this, is_array($names) ? $names : func_get_args())
+        );
+    }
+
+    public function getCacheName()
+    {
+        return $this->cacheName;
+    }
+
+}
