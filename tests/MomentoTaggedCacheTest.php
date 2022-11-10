6c227d4a53e7d3df400db3df6989b79aa5c585db
erikatharp <eem@momentohq.com>
2022-11-10 12:34:22 -0800

    chore: add tests to address tagged cahe scenarios

--- tests/MomentoTaggedCacheTest.php
+++ tests/MomentoTaggedCacheTest.php
@@ -38,7 +38,7 @@ class MomentoTaggedCacheTest extends BaseTest
     /**
      * @covers \Momento\Cache\MomentoTaggedCache
      */
-    public function testTaggedPutMultipleItemsWithOneTag_HappyPath()
+    public function testTaggedPutMultipleItems_WithOneTag_HappyPath()
     {
         $key1 = uniqid();
         $value1 = uniqid();
@@ -53,5 +53,48 @@ class MomentoTaggedCacheTest extends BaseTest
         $this->assertEquals($value1, $getResult, "${value1} was expected but received ${getResult}");
         $getResult = Cache::tags([$tag1])->get($key2);
         $this->assertEquals($value2, $getResult, "${value2} was expected but received ${getResult}");
+    }
+
+    /**
+     * @covers \Momento\Cache\MomentoTaggedCache
+     * @covers \Momento\Cache\MomentoStore
+     */
+    public function testTaggedPut_RegularGet_IsNull()
+    {
+        $key = uniqid();
+        $value = uniqid();
+        $tag = uniqid();
+        $putResult = Cache::tags([$tag])->put($key, $value, 5);
+        $this->assertTrue($putResult, "True was expected but received ${putResult}");
+        $getResult = Cache::get($key);
+        $this->assertNull($getResult, "null was expected but received ${getResult}");
+    }
+
+    /**
+     * @covers \Momento\Cache\MomentoTaggedCache
+     * @covers \Momento\Cache\MomentoStore
+     */
+    public function testTaggedPut_WithWrongOrderTaggedGet_IsNull()
+    {
+        $key = uniqid();
+        $value = uniqid();
+        $tag1 = uniqid();
+        $tag2 = uniqid();
+        $putResult = Cache::tags([$tag1, $tag2])->put($key, $value, 5);
+        $this->assertTrue($putResult, "True was expected but received ${putResult}");
+        $getResult = Cache::tags([$tag2, $tag1])->get($key);
+        $this->assertNull($getResult, "null was expected but received ${getResult}");
+    }
+
+    public function testTaggedPutWithTwoTags_TaggedGetWithOnlyOneTag_IsNull()
+    {
+        $key = uniqid();
+        $value = uniqid();
+        $tag1 = uniqid();
+        $tag2 = uniqid();
+        $putResult = Cache::tags([$tag1, $tag2])->put($key, $value, 5);
+        $this->assertTrue($putResult, "True was expected but received ${putResult}");
+        $getResult = Cache::tags([$tag1])->get($key);
+        $this->assertNull($getResult, "null was expected but received ${getResult}");
     }
 }
