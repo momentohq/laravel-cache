342f13f0a0278b79469ca48b8eb580f4ed699dd3
rishtigupta <127137312+rishtigupta@users.noreply.github.com>
2024-09-16 14:11:53 -0700

    chore: add create cache and delete cache before/after tests (#42)

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -2,12 +2,19 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
+use Momento\Auth\EnvMomentoTokenProvider;
+use Momento\Cache\CacheClient;
+use Momento\Cache\Errors\InvalidArgumentError;
 use Momento\Cache\Errors\UnknownError;
 use Momento\Cache\MomentoServiceProvider;
 use Momento\Cache\MomentoStore;
+use Momento\Config\Configurations\Laptop;
 
 abstract class BaseTest extends Orchestra\Testbench\TestCase
 {
+    protected static CacheClient $cacheClient;
+    protected static string $cacheName;
+
     /**
      * Get package providers.
      *
@@ -24,6 +31,7 @@ abstract class BaseTest extends Orchestra\Testbench\TestCase
 
     /**
      * Setup the test environment.
+     * @throws UnknownError
      */
     protected function setUp(): void
     {
@@ -43,8 +51,44 @@ abstract class BaseTest extends Orchestra\Testbench\TestCase
                 'default_ttl' => 60
             ]
         );
-
-
     }
 
+    /**
+     * Setup before any test in the class runs.
+     * @throws InvalidArgumentError
+     */
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+        $cacheName = getenv('MOMENTO_CACHE_NAME');
+        $authProvider = new EnvMomentoTokenProvider('MOMENTO_API_KEY');
+        $configuration = Laptop::latest();
+        self::$cacheClient = new CacheClient($configuration, $authProvider, 60);
+        self::$cacheName = $cacheName;
+        $result = self::$cacheClient->createCache($cacheName);
+
+        if ($result->asSuccess()) {
+            error_log("Cache '$cacheName' created successfully.");
+        } else if ($result->asAlreadyExists()) {
+            error_log("Cache '$cacheName' already exists.");
+        } else if ($result->asError()) {
+            error_log("Error creating cache '$cacheName': " . $result->asError()->message());
+            throw new RuntimeException("Error creating cache '$cacheName': " . $result->asError()->message());
+        }
+    }
+
+    /**
+     * Clean up after all tests in the class have run.
+     */
+    public static function tearDownAfterClass(): void
+    {
+        parent::tearDownAfterClass();
+        $result = self::$cacheClient->deleteCache(self::$cacheName);
+
+        if ($result->asSuccess()) {
+            error_log("Cache '" . self::$cacheName . "' deleted successfully.");
+        } else if ($result->asError()) {
+            error_log("Error deleting cache '" . self::$cacheName . "': " . $result->asError()->message());
+        }
+    }
 }
